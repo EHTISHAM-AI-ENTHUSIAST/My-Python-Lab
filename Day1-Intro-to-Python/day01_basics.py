e8d82c53651ab7187c4bb01f8f239a475a9e1693
Ehtisham Ashraf <kingehtisham0@gmail.com>
2025-10-30 18:11:40 +0500

    Added Day 1 practice files

---
+++ Day1-Intro-to-Python/day01_basics.py
@@ -0,0 +1,17 @@
+# Day 1 - Introduction to Python
+
+# Author: Your Name
+
+# Date: October 30, 2025
+
+# 1. Basic Print Statement
+
+print("Welcome to Day 1 of Python Journey! 🐍")
+
+# 2.input
+
+name=input("Enter your name: ")
+
+print("welcome Brother",name)
+
+# This is just the beginning! Keep learning and exploring Python! 🚀
