5f096c4dbe63806977c9efc1b1c4a913289d0464
Ehtisham Ashraf <kingehtisham0@gmail.com>
2025-12-28 13:51:13 +0500

    Python

---
+++ 14.break & continue/main.py
@@ -0,0 +1,17 @@
+# continue statement:
+
+# for i in range(1, 13):
+#   if(i == 10):
+#     print("Skip the iteration")
+#     continue
+#   print("5 X", i, "=", 5 * i)
+
+
+
+# break statement:
+
+for i in range(1, 13):
+  if(i == 10):
+    print("Exit the loop")
+    break
+  print("5 X", i, "=", 5 * i)
